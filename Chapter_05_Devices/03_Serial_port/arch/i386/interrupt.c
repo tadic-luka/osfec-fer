55a099444b77c3dfd93f58ec4556fe99378b3132
Luka Tadic <luka.tadic@vingd.com>
2018-05-08 07:25:56 +0200

    made priority interrupt handling

--- Chapter_05_Devices/03_Serial_port/arch/i386/interrupt.c
+++ Chapter_05_Devices/03_Serial_port/arch/i386/interrupt.c
@@ -12,6 +12,19 @@
 extern arch_ic_t IC_DEV;
 static arch_ic_t *icdev = &IC_DEV;
 
+/*! For priority handling */
+static int prio[INTERRUPTS];
+static list_t reqs;
+static int in_process = 0;
+struct request {
+	int priority;
+	int in_process;
+	void *device;
+	int (*ihandler) (unsigned int, void *device);
+	int irq;
+	list_h list;
+};
+
 /*! interrupt handlers */
 static list_t ihandlers[INTERRUPTS];
 
@@ -23,6 +36,17 @@ struct ihndlr
 	list_h list;
 };
 
+int cmp_reqs_by_priority(void *a, void *b)
+{
+	struct request *req_a = (struct request *)a;
+	struct request *req_b = (struct request *)b;
+
+	if(req_a->priority < req_b->priority)
+		return -1;
+	if(req_a->priority == req_b->priority)
+		return 1;
+	return 0;
+}
 /*! Initialize interrupt subsystem (in 'arch' layer) */
 void arch_init_interrupts ()
 {
@@ -30,8 +54,13 @@ void arch_init_interrupts ()
 
 	icdev->init ();
 
-	for ( i = 0; i < INTERRUPTS; i++ )
+	for ( i = 0; i < INTERRUPTS; i++ ) {
 		list_init ( &ihandlers[i] );
+		// random_shuffle
+		prio[i] = (i * 22) % INTERRUPTS;
+	}
+	// initalize list of reqs
+	list_init(&reqs);
 }
 
 /*!
@@ -52,7 +81,6 @@ void arch_register_interrupt_handler ( unsigned int inum, void *handler,
 				       void *device )
 {
 	struct ihndlr *ih;
-
 	if ( inum < INTERRUPTS )
 	{
 		ih = kmalloc ( sizeof (struct ihndlr) );
@@ -98,8 +126,10 @@ void arch_interrupt_handler ( int irq_num )
 {
 	struct ihndlr *ih;
 
+	struct request *req;
 	if ( irq_num < INTERRUPTS && (ih = list_get (&ihandlers[irq_num], FIRST)) )
 	{
+		LOG ( INFO, "IRQ: %d", irq_num);
 		/* enable interrupts on PIC immediately since program may not
 		 * return here immediately */
 		if ( icdev->at_exit )
@@ -108,10 +138,41 @@ void arch_interrupt_handler ( int irq_num )
 		/* Call registered handlers */
 		while ( ih )
 		{
-			ih->ihandler ( irq_num, ih->device );
+			/*ih->ihandler ( irq_num, ih->device );*/
+			req = kmalloc( sizeof(struct request) + 100000);
+			ASSERT( req );
+
+			req->priority = prio[irq_num];
+			req->in_process = 0;
+			req->device = ih->device;
+			req->ihandler = ih->ihandler;
+			req->irq = irq_num;
+			LOG(INFO, "REQ %d inserted, in_process: %d", irq_num, in_process);
+
+			list_sort_add (&reqs , req, &req->list, cmp_reqs_by_priority );
 
 			ih = list_get_next ( &ih->list );
 		}
+
+		while((req = list_get(&reqs, FIRST)) && req->in_process == 0) {
+			/*LOG(DEBUG, "GOT req with irq: %d, prio: %d", req->irq, req->priority);*/
+			req->in_process = 1;
+			++in_process;
+			enable_interrupts();
+			/*arch_irq_enable(req->irq);*/
+			req->ihandler(req->irq, req->device);
+			/*arch_irq_disable(req->irq);*/
+			disable_interrupts();
+			req = list_remove(&reqs, FIRST, NULL);
+			if( req ) {
+				LOG(INGO, "REQ %d removed", req->irq);
+				--in_process;
+				kfree(req);
+			}
+		}
+		/*arch_irq_disable(irq_num);*/
+		/*arch_irq_enable(irq_num);*/
+			/*disable_interrupts();*/
 	}
 
 	else if ( irq_num < INTERRUPTS )
